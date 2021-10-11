606d82473564cc6402c6445c82675ea673ee9df3
Zhensong Ren <zhensong.ren@gmail.com>
2021-10-11 20:30:24 +0000

    Cleaned up the main python file

--- 1p3a_interview_DS_US_scraper.py
+++ 1p3a_interview_DS_US_scraper.py
@@ -86,79 +86,36 @@ def get_referrals(driver, START_URL, max_page_num=10, latest_id_from_CSV=None, l
         driver.get(url)
         soup = BeautifulSoup(driver.page_source, 'lxml')
         posts = soup.find_all('tbody', id=re.compile("normalthread_")) 
-#         print(f'{len(posts)} posts to be scraped')
         # get list of referral data for each page
         for p in posts:
-
             # Date
             date = get_date(p)
-
             # Poster. 有匿名用户。
             poster = get_poster(p)
-
             # 回复
             reply = get_reply_num(p)
             # 查看
             view = get_view_num(p)
-
             # post id
             post_id = get_post_id(p)
-
             # Check if the post already saved.   
             if  post_id==latest_id_from_CSV:
                 get_next_page = False
                 break   
-
             # post_title
             post_title = get_post_title(p)
             # company
             company = get_company(p)
             post_dict = {'Date': date, 'Poster': poster, 'Replies': reply, 'Views': view, 'post_id':post_id, 'post_title':post_title, 'company':company}
-#             print(post_dict)
-#             print('--'*20)
+            # print(post_dict)
+            # print('--'*20)
             refers.append(post_dict)
         # set the next page
         i+=1
     return refers
-    
-    
-# # Insert the new data to the front of the csv file.
-# def init_csv_file(file_path, field_names):
-#     with open(file_path, 'w', newline='', encoding='utf8') as csv_file:
-#         writer = csv.DictWriter(csv_file, fieldnames=field_names)
-#         writer.writeheader()
-        
-# def write_to_csv(file_path, field_names, data):
-#     with open(file_path, 'a', newline='', encoding='utf8') as csv_file:
-#         writer = csv.DictWriter(csv_file, fieldnames=field_names)
-#         writer.writerows(data)
-
-# field_names = ['Date', 'Poster', 'Replies', 'Views', 'post_id', 'post_title', 'company']
-
-# #initialize the file for the 1st scrpae
-# init_csv_file('./referral_US.csv', field_names=field_names)
-# #Add new data to existing file
-# write_to_csv('./referral_US.csv', field_names=field_names, data=refers)
-
-# Lower level of handling saving new data.
-def insert_into_csv(file_path, data):
-    with open(file_path, 'r', newline='', encoding='utf8') as csv_file:
-        text = csv_file.readlines()
-        new_data = []
-        for row_num in range(len(refers)):
-            row = ','.join([str(value) for value in data[row_num].values()])+'\n'
-            new_data.append(row)
-        # Use slice assignment to insert a list to the first row.
-        text[0:0] = new_data
-        print(text[0])
-        
-    with open(file_path, 'w', newline='', encoding='utf8') as csv_file:
-        csv_file.writelines(text)
-
-# insert_into_csv('./referral_US.csv', data=refers)
 
 # Always save the new data just below the header row.
-def insert_data(file_path, data):
+def save_data_to_existing_file(file_path, data):
     fieldnames = []
     if len(data)>0:
         fieldnames = list(data[0].keys())
@@ -196,21 +153,22 @@ if os.path.exists('./interview_DS_US.csv'):
         latest_id_from_CSV = 0
         latest_date_from_CSV = pd.to_datetime('1900-01-01')
 
-
+# scrape without chorme UI(the headless option)
 chrome_options = set_chrome_options()
 driver = webdriver.Chrome(options=chrome_options)
-# driver = webdriver.Chrome()
+
+# scrape with chorme UI
+# driver = webdriver.Chrome() 
 START_URL = "https://www.1point3acres.com/bbs/forum.php?mod=forumdisplay&fid=259&orderby=dateline&sortid=311&filter=author&orderby=dateline&sortid=311&page="
 
 refers = get_referrals(driver, START_URL, max_page_num=50, 
                        latest_id_from_CSV=latest_id_from_CSV, latest_date_from_CSV=latest_date_from_CSV)
 
-insert_data('./interview_DS_US.csv', data=refers)
+save_data_to_existing_file('./interview_DS_US.csv', data=refers)
 
 df = pd.read_csv('interview_DS_US.csv')
-
+print('---'*50)
 print(df.head())
-
+print('---'*50)
 print(df.shape)
-
 print(df.dtypes)
